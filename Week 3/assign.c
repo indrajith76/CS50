60825edd3585b64af7e8a9ec24b2d66522ec40b3
indrajith76 <indrajithgoswami@gmail.com>
2023-11-02 02:49:12 +0600

    added PCIU task

--- Week 3/assign.c
+++ Week 3/assign.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 int main()
 {
+    int number;
+    printf("Enter Number: ");
+    scanf("%i", &number);
+    for (int i = 1; i <= number; i++)
+    {
+        for (int j = number; j > i; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k <= 2 * (i - 1); k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 1; i < number; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 2 * (number - i); k > 1; k--)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    /* Output:
+     *
+     ***
+     *****
+     *******
+     *********
+     *******
+     *****
+     ***
+     *
+     */
+
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j <= i; j++)
@@ -9,18 +50,7 @@ int main()
         }
         printf("\n");
     }
-    /* Output is:
-        #
-        ##
-        ###
-        ####
-        #####
-        ######
-        #######
-        ########
-        #########
-        ########## 
-    */
+
     for (int i = 10; i >= 1; i--)
     {
         for (int j = 0; j <= i - 1; j++)
@@ -29,16 +59,4 @@ int main()
         }
         printf("\n");
     }
-    /* Output is: 
-        ##########
-        #########
-        ########
-        #######
-        ######
-        #####
-        ####
-        ###
-        ##
-        #
-     */
 }
